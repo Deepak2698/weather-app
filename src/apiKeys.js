4a1b919f2f031ef5950531778cdff6f95f36ba46
DEEPAK MISHRA <you@example.com>
2023-07-15 15:35:54 +0530

    new

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "c10201fe9e02dc1d61ec6cde30a328ed",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
